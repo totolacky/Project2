eb2f1d5a9b76c574eefb4634d0a7da9fa0990fbd
JaneYi <janeyi@naver.com>
2020-01-03 16:57:57 +0900

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,70 @@
-aaa
+// Import package
+var mongodb = require('mongodb');
+var ObjectID = mongodb.ObjectID;
+var crypto = require('crypto')
+var express = require('express')
+var bodyParser = require('body-parser')
+
+// PASSWORD UTILS
+// create function to random salt
+var genRandomString = function(length){
+    return crypto.randomBytes(Math.ceil(length/2)).toString('hex').slice(0,length);
+}
+
+var sha512 = function(password, salt){
+    var hash = crypto.createHmac('sha512',salt);
+    hash.update(password);
+    var value = hash.digest('hex');
+    return{
+        salt:salt,
+        passwordHash:value
+    };
+};
+
+function saltHashPassword(userPassword){
+    var salt = genRandomString(16);
+    var passwordData = sha512(userPassword,salt);
+    return passwordData;
+}
+
+function checkHashPassword(userPassword,salt){
+    var passwordData = sha512(userPassword,salt);
+    return passwordData;
+}
+
+// Create Express Service
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Create MongoDB Client
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL
+var url = 'mongodb://localhost:27017'
+
+MongoClient.connect(url, {useNewUrlParser: true}, function(err,client){
+    if(err) console.log('Unable to connection to the mongoDB server.Error', err);
+    else{
+        /*// Register
+        app.post('/register', (request,response,next)=>{
+            var post_data = request.body;
+
+            var plaint_password = post_data.password;
+            var hash_data = saltHashPassword(plaint_password);
+
+        });*/
+
+        // Start Web Server
+        app.listen(3000, ()=>{
+            console.log('Connected to MongoDB Server , WebService running on port 3000')
+        })
+    }
+})
+
+
+
+
+
+
+
